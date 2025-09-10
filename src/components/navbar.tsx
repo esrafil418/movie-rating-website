19e325fde6ced6f4205b37d3b0f6a078924bed2f
esrafil <esrafil.s.r.man418@gmail.com>
2025-09-10 10:54:19 +0330

    Added: Login option in Auth page
    -Login option added to Auth page
    -Clicking the login button will redirect the user to the Home page
    Navigation updated
    Tmdb site api fetched

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,10 +4,18 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
   return (
     <Menu fixed="top" size="huge">
+      {/* Left side of Navbar  */}
+
+      <Menu.Item as={Link} to="/" style={{ fontSize: "1.5rem" }}>
+        Home
+      </Menu.Item>
+      <Menu.Item as={Link} to="/rated" style={{ fontSize: "1.5rem" }}>
+        Rated
+      </Menu.Item>
+      {/* Right side of Navbar */}
       <Menu.Menu position="right">
-        <Menu.Item as={Link} to="/auth">
-          {" "}
-          Auth{" "}
+        <Menu.Item as={Link} to="/auth" style={{ fontSize: "1.5rem" }}>
+          Auth
         </Menu.Item>
       </Menu.Menu>
     </Menu>
---
+++ src/pages/auth/mutation.ts
@@ -0,0 +1,13 @@
+export const mutationLogin = async () => {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/authentication/guest_session/new",
+    {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzBjNTUxYmMxNzJhNDJhNjQ2Yzk1Y2VkZTYxODIyOCIsIm5iZiI6MTUzNjU5ODIyMS40MjIwMDAyLCJzdWIiOiI1Yjk2YTBjZGMzYTM2ODU2NzkwM2Q4MTciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.vNJkjF6ot7s8UCmLgTYzOE4lCZp7NZ8rj6wGss8G2WQ",
+      },
+    }
+  );
+  console.log(res.json());
+  return res.json();
+};
